3ff033988322321a983d2b0c8511b44643787c84
Yuto Suzuki <mokkumokku99@gmail.com>
2022-08-02 19:18:38 -0600

    adapt to all datasets

--- script_mask_analysis.sh
+++ script_mask_analysis.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python mask_analysis.py --num_users 10 --algorithm fedspa --dataset cifar10 --percentage 0.2 --top True --seed 3 --layers_from_last 1
+python mask_analysis.py --num_users 100 --algorithm ours --dataset cifar10 --percentage 0.2 --top True --seed 1 
 
